0cd596d38cf5ddef72d554625ba1f3f4ad7d4e06
Akraja712 <ashokkumar7hitter@gmail.com>
2024-04-09 21:21:21 +0530

    crud.php

---
+++ api/user_details.php
@@ -0,0 +1,43 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Content-Type: application/json");
+header("Expires: 0");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+include_once('../includes/crud.php');
+
+$db = new Database();
+$db->connect();
+
+
+if (empty($_POST['user_id'])) {
+    $response['success'] = false;
+    $response['message'] = "User ID is Empty";
+    echo json_encode($response);
+    return false;
+}
+
+$user_id = $db->escapeString($_POST['user_id']);
+
+$sql_user = "SELECT * FROM users WHERE id = $user_id";
+$db->sql($sql_user);
+$res_user = $db->getResult();
+$num = $db->numRows($res_user);
+
+if ($num >= 1) {
+    
+    $user_details = $res_user[0];
+    $user_details['profile'] = DOMAIN_URL . $user_details['profile'];
+    $response['success'] = true;
+    $response['message'] = "User Details Retrieved Successfully";
+    $response['data'] = array($user_details);
+    echo json_encode($response);
+} else {
+    $response['success'] = false;
+    $response['message'] = "User Not found";
+    echo json_encode($response);
+}
+?>
