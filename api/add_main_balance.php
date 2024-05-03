e35623250bb2e087d44ac05eee7d42b91fa52bec
Akraja712 <ashokkumar7hitter@gmail.com>
2024-05-03 10:31:25 +0530

    add_main_balance.php

--- api/add_main_balance.php
+++ api/add_main_balance.php
@@ -25,94 +25,59 @@ if (empty($_POST['wallet_type'])) {
     print_r(json_encode($response));
     return false;
 }
+
 $datetime = date('Y-m-d H:i:s');
-$currentDate = date('Y-m-d');
-$firstDateOfMonth = date('Y-m-01', strtotime('today'));
 $user_id=$db->escapeString($_POST['user_id']);
 $wallet_type = $db->escapeString($_POST['wallet_type']);
+
+function isBetween10AMand6PM() {
+    $currentHour = date('H');
+    $startTimestamp = strtotime('10:00:00');
+    $endTimestamp = strtotime('18:00:00');
+    return ($currentHour >= date('H', $startTimestamp)) && ($currentHour < date('H', $endTimestamp));
+}
 
 $sql = "SELECT * FROM users WHERE id = '" . $user_id . "'";
 $db->sql($sql);
 $res = $db->getResult();
 $num = $db->numRows($res);
 
-
 if ($num == 1) {
-    //$today_orders = $res[0]['today_orders'];
-    //$status = $res[0]['status'];
-    $hiring_earings = $res[0]['hiring_earings']; 
-    $orders_earnings = $res[0]['orders_earnings'];
-    $average_orders = $res[0]['average_orders'];
-    //$student_plan = $res[0]['student_plan'];
-    //$days_60_plan = $res[0]['days_60_plan'];
-    $total_referrals = $res[0]['total_referrals'];
+    $earning_wallet = $res[0]['earning_wallet']; 
+    $bonus_wallet = $res[0]['bonus_wallet'];
 
-    if($wallet_type == 'hiring_earings'){
-        if ($hiring_earings < 100) {
+    if($wallet_type == 'earning_wallet'){ 
+        if ($earning_wallet < 100) {
             $response['success'] = false;
-            $response['message'] = "Minimum 100 rs to add";
+            $response['message'] = "Minimum 500 rs to add";
             print_r(json_encode($response));
             return false;
         }
-        $sql = "INSERT INTO transactions (`user_id`,`type`,`datetime`,`amount`) VALUES ($user_id,'hiring_earings','$datetime',$hiring_earings)";
-        $db->sql($sql);
-        $sql = "UPDATE users SET hiring_earings= hiring_earings - $hiring_earings,earn = earn + $hiring_earings,balance = balance + $hiring_earings  WHERE id=" . $user_id;
-        $db->sql($sql);
-    }
-    if($wallet_type == 'orders_earnings'){
-        if ($orders_earnings < 100) {
+
+        if (!isBetween10AMand6PM()) {
             $response['success'] = false;
-            $response['message'] = "Minimum 100 rs to add";
+            $response['message'] = "Add Wallet time morning 10:00AM to 6PM";
             print_r(json_encode($response));
             return false;
         }
-       /* if($student_plan == 0){
-            $today = date("N");
-            if($average_orders >= 400 && $average_orders < 500 && $today != 1){
-                $response['success'] = false;
-                $response['message'] = "Enable on Monday only";
-                print_r(json_encode($response));
-                return false;
-    
-            }
-            if ($average_orders >= 300 && $average_orders < 400 && $currentDate != $firstDateOfMonth) {
-                $response['success'] = false;
-                $response['message'] = "Enable on First date of the month";
-                print_r(json_encode($response));
-                return false;
-            }
-    
-    
-            if($average_orders < 300 ){
-                $response['success'] = false;
-                $response['message'] = "Disabled";
-                print_r(json_encode($response));
-                return false;
-    
-            }
-        }
-        if ($days_60_plan == 1) {
-            if ($total_referrals >= 0 && $total_referrals <= 3 && $currentDate != $firstDateOfMonth) {
-                $response['success'] = false;
-                $response['message'] = "Enable on First date of the month";
-                print_r(json_encode($response));
-                return false;
-            } 
-            if ($total_referrals >= 4 && $total_referrals <= 6) {
-                $response['success'] = false;
-                $response['message'] = "Enable on Monday only";
-                print_r(json_encode($response));
-                return false;
-    
-            } 
-        }*/
-        
-        $sql = "INSERT INTO transactions (`user_id`,`type`,`datetime`,`amount`) VALUES ($user_id,'orders_earnings','$datetime',$orders_earnings)";
+
+        $sql = "INSERT INTO transactions (`user_id`,`type`,`datetime`,`amount`) VALUES ($user_id,'earning_wallet','$datetime',$earning_wallet)";
         $db->sql($sql);
-        $sql = "UPDATE users SET orders_earnings= orders_earnings - $orders_earnings,earn = earn + $orders_earnings,balance = balance + $orders_earnings  WHERE id=" . $user_id;
+        $sql = "UPDATE users SET earning_wallet= earning_wallet - $earning_wallet,balance = balance + $earning_wallet  WHERE id=" . $user_id;
         $db->sql($sql);
     }
-
+    if($wallet_type == 'bonus_wallet'){
+        if ($bonus_wallet < 50) {
+            $response['success'] = false;
+            $response['message'] = "Minimum 50 rs to add";
+            print_r(json_encode($response));
+            return false;
+        }
+        $sql = "INSERT INTO transactions (`user_id`,`type`,`datetime`,`amount`) VALUES ($user_id,'bonus_wallet','$datetime',$bonus_wallet)";
+        $db->sql($sql);
+        $sql = "UPDATE users SET bonus_wallet= bonus_wallet - $bonus_wallet,balance = balance + $bonus_wallet  WHERE id=" . $user_id;
+        $db->sql($sql);
+    }
 
 
     $sql = "SELECT * FROM users WHERE id = '" . $user_id . "'";
